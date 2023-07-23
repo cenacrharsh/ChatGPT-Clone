3b9c6f2c82b622551274d7b8e9682da6016c1970
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-07-23 21:32:05 +0530

    calling chat gpt api from frontend and server

--- samples/public/playground/app.js
+++ samples/public/playground/app.js
@@ -1,16 +1,13 @@
-
 async function send() {
     const prompt = document.querySelector("#prompt").value;
 
     const response = await fetch("/api/general", {
-        method: 'POST',
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
         },
-        body: JSON.stringify({prompt})
-    })
+        body: JSON.stringify({ prompt }),
+    });
     const output = await response.json();
     document.querySelector("output").textContent = output;
-
-
 }
--- samples/server.js
+++ samples/server.js
@@ -1,71 +1,67 @@
-
-
-const OPENIA_KEY = "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";
-
-const path = require('path');
-const express = require('express');
-const cors = require('cors');
-const app = express();
-
-app.use(express.json()); // parse JSON requests
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.post('/api/chat', async (req, res) => {
-  const body = req.body;
-  const response = await fetch('https://api.openai.com/v1/chat/completions', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer sess-vpiF9RZH2qkkmfXscAZUS3nIFIVdSDzw00jMOxAC',
-    },
-    body: JSON.stringify({
-        ...body,
-        temperature: 0,
-        max_tokens: 100,
-        model: 'gpt-3.5-turbo',
-        stream: false
-    })
-  });
-  res.json(await response.json());
-});
-
-
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 const { Configuration, OpenAIApi } = require("openai");
 
+const app = express();
+dotenv.config();
+
+// ChatGPT Setup
 const configuration = new Configuration({
-  apiKey: OPENIA_KEY,
+    apiKey: process.env.OPENAI_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
+app.use(express.json()); // parse JSON requests
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.post('/api/general', async (req, res) => {
-  const body = req.body;
-
-  const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{role: "user", content: body.prompt}],
-  });
-  res.json(completion.data.choices[0].message.content);
+app.post("/api/chat", async (req, res) => {
+    const body = req.body;
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.OPENAI_KEY}`,
+        },
+        body: JSON.stringify({
+            ...body,
+            temperature: 0,
+            max_tokens: 100,
+            model: "gpt-3.5-turbo",
+            stream: false,
+        }),
+    });
+    res.json(await response.json());
 });
 
-app.post('/api/image', async (req, res) => {
-  const body = req.body;
+app.post("/api/general", async (req, res) => {
+    const body = req.body;
 
-  const response = await openai.createImage({
-    prompt: body.prompt,
-    n: 1,
-    size: "512x512",
-  });
-  image_url = response.data.data[0].url;  
-  res.json({"url": image_url});
-})
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: body.prompt }],
+    });
+    res.json(completion.data.choices[0].message.content);
+});
 
-app.post('/api/recipe', async (req, res) => {
-  const body = req.body;
+app.post("/api/image", async (req, res) => {
+    const body = req.body;
 
-  const prompt = `Give me a recipe with the list of ingredientes defined in the markup
+    const response = await openai.createImage({
+        prompt: body.prompt,
+        n: 1,
+        size: "512x512",
+    });
+    image_url = response.data.data[0].url;
+    res.json({ url: image_url });
+});
+
+app.post("/api/recipe", async (req, res) => {
+    const body = req.body;
+
+    const prompt = `Give me a recipe with the list of ingredientes defined in the markup
   <ingredients>${body.ingredients}</ingredients>
   You can take for granted other basic ingredients, such as salt, pepper and other condiments we usually find in a kitchen.  
   
@@ -99,37 +95,37 @@ If one step's description has a time declaration like 'for 30 minutes', add the 
                 "timer": 25
             },
             {
-              "name": "Wait",
-              "description": "Wait 5 minutes.",
-              "timer": 5
-          }
-           
+                "name": "Wait",
+                "description": "Wait 5 minutes.",
+                "timer": 5
+            }
         ]
     }
 ***
-`
+`;
 
-  const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content: "You are a professional chef who recommends recipes bases on ingredients"
-      },
-      {
-        role: "user",
-        content: prompt
-      }
-    ],
-    temperature: 0.7,
-    max_tokens: 1000,
-    model: 'gpt-3.5-turbo',
-    stream: false
-  });
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+            {
+                role: "system",
+                content:
+                    "You are a professional chef who recommends recipes bases on ingredients",
+            },
+            {
+                role: "user",
+                content: prompt,
+            },
+        ],
+        temperature: 0.7,
+        max_tokens: 1000,
+        model: "gpt-3.5-turbo",
+        stream: false,
+    });
 
-  res.json({content: completion.data.choices[0].message.content});
+    res.json({ content: completion.data.choices[0].message.content });
 });
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+    console.log("Server started on port 3000");
 });
